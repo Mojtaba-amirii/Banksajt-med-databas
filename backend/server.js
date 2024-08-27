6e557e838655407850c38b3a066fb9e56c649649
mojtabaamiri <mojtaba.a.amiri@outlook.com>
2024-08-27 15:14:46 +0200

    fixing 8

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,17 @@ function authenticateToken(req, res, next) {
 const app = express();
 const PORT = process.env.PORT || 4001;
 
-app.use("*", cors());
+app.use(
+  cors({
+    origin: [
+      "https://banksajt-med-databas.vercel.app",
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
 
 // Endpoint to add a new user
