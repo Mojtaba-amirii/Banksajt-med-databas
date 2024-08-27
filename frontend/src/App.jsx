75fcb02751fec919c0ac5b1ad04dc92435731eac
mojtabaamiri <mojtaba.a.amiri@outlook.com>
2024-08-27 14:10:06 +0200

    fixed 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,8 +9,6 @@ function App() {
   const [registrationStatus, setRegistrationStatus] = useState(null);
 
   function handleRegister() {
-    console.log("API Base URL:", import.meta.env.VITE_API_BASE_URL);
-
     const user = {
       username: username,
       password: password,
