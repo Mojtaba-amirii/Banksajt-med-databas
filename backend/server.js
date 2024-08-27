5f7da6d6d728f79642866e5183ac4b4763274913
mojtabaamiri <mojtaba.a.amiri@outlook.com>
2024-08-27 17:43:46 +0200

    fix 1

--- backend/server.js
+++ backend/server.js
@@ -31,12 +31,13 @@ const app = express();
 const PORT = process.env.PORT || 4001;
 
 app.use(
-  cors("*", {
+  cors({
     origin: [
       "https://banksajt-med-databas.vercel.app",
       "http://localhost:5173",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
